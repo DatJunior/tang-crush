f0d58adb29da7c8cea05719732edf67e14ae2958
DatJunior <85281696+DatJunior@users.noreply.github.com>
2021-06-03 15:22:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Con chym bé bỏng của a à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
